afe1da5f9e59326bed080fcd5d88999498f6bd1f
Joe Shepherd <joeshepmedia@gmail.com>
2017-08-02 22:42:54 -0500

    Add comments

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-'use strict'
+'use strict';
+
+// To run this app:
+// Run the PG server
+// On the CL, type `npm run start:dev` then go to localhost:8000/api
 
 const express = require('express');
 const logger = require('morgan');
@@ -14,8 +18,8 @@ app.use(logger('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// We have to require our routes before the app.get('*', ...) catch-all route 
-// because the catch-all route will match any route and serve the welcome message, 
+// We have to require our routes before the app.get('*', ...) catch-all route
+// because the catch-all route will match any route and serve the welcome message,
 // If we require our other routes after it, those other routes will never be hit.
 app.use(routes);
 // Default route runs on every request and logs a greeting
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
-// If we don't have a Node environment defined, we're defaulting to development. 
-// Then, we are establishing a connection with our database, 
-// after which we read our models folder, discovering and importing any and all the models in it, 
+// If we don't have a Node environment defined, we're defaulting to development.
+// Then, we are establishing a connection with our database,
+// after which we read our models folder, discovering and importing any and all the models in it,
 // adding them to the db object and applying relationships between the models, if such relationships exist.
 
 'use strict';
@@ -14,12 +14,18 @@ const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
 
 let sequelize;
+
+// Sequelize does setup a connection between the rest api/application
+// and your SQL database. To setup basic connection between the two:
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// Reading all the files in this directory (/models) and adding them to the db object
+// so we can export a single 'db' object from this file and have access to
+// all the models we have defined
 fs
   .readdirSync(__dirname)
   .filter((file) => {
